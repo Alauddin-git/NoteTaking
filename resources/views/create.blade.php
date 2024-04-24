9114483e57e38c39d2828b2db68175d7e50b4fb3
Emon-git-site <165896056+Alauddin-git@users.noreply.github.com>
2024-04-24 18:21:33 +0600

    create template done

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -6,31 +6,33 @@
         <div class="container-fluid">
             <!-- general form elements -->
             <div class="card card-primary mt-2">
-                <div class="card-header">
+                <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
                     <h3 class="card-title">Add New Note</h3>
+                    <button onclick="window.history.back();" class="btn btn-secondary">Back</button>
                 </div>
                 <!-- /.card-header -->
                 <!-- form start -->
                 <form action="" method="post">
                     @csrf
                     <div class="card-body">
-                        <div class="form-group">
-                            <label for="name">Exam Name</label>
-                            <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Exam Name" required>
+                        <div class="form-group mb-3">
+                            <label class="fw-bold" for="name">Note Title</label>
+                            <input type="text" class="form-control mt-2" name="name" value="{{ old('name') }}" placeholder="Enter Note Ttile" required>
                             <div style="color: red">{{ $errors->first('name') }}</div>
                         </div>
                         <div class="form-group">
-                            <label for="email">Note</label>
-                            <textarea class="form-control" name="note" cols="30" rows="5"></textarea>
+                            <label class="fw-bold" for="note">Note Content</label>
+                            <textarea class="form-control mt-2" name="note" cols="30" rows="5"></textarea>
                         </div>
                     </div>
                     <!-- /.card-body -->
-
+            
                     <div class="card-footer">
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </div>
                 </form>
             </div>
+            
         </div>
     </section>
 </div>
